0f6471d770295d7345e708e5cecf14ee79d42bd9
Jean-Baptiste Delvaux <jb.delvaux@technofutur.be>
2025-10-27 09:42:11 +0100

    New Correct Init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    /*{
+        path:'home',
+        component: Home
+    },*/
+    {
+        path:'lazyHome',
+        loadComponent:()=>import('./features/home/home').then(m => m.Home)
+    }
+];
